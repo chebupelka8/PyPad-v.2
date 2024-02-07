2251304fded2fe6dc52f45e3606c66d8189da717
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2024-02-07 11:10:27 +0500

    Add files via upload

--- scr/subwidgets/dialog.py
+++ scr/subwidgets/dialog.py
@@ -1,6 +1,6 @@
 from scr.scripts import FileLoader, restart_application
 
-from PySide6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QPushButton, QLabel
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit
 from PySide6.QtCore import Qt
 
 
@@ -8,7 +8,6 @@ class _Dialog(QDialog):
     def __init__(self, __parent, __message: str, accept_title: str = "Ok", reject_title: str = "Cancel") -> None:
         super().__init__(__parent, f=Qt.WindowType.FramelessWindowHint)
 
-        self.setWindowTitle("PyPad")
         self.setStyleSheet(FileLoader.load_style("scr/styles/dialog.css"))
 
         self.mainLayout = QVBoxLayout()
@@ -17,6 +16,47 @@ class _Dialog(QDialog):
         self.mainLayout.addWidget(
             QLabel(__message), alignment=Qt.AlignmentFlag.AlignCenter
         )
+        self.mainLayout.addLayout(self.buttonLayout)
+
+        self.acceptBtn = QPushButton(accept_title)
+        self.acceptBtn.setObjectName("accept-btn")
+        self.rejectBtn = QPushButton(reject_title)
+        self.rejectBtn.setObjectName("reject-btn")
+
+        self.acceptBtn.clicked.connect(self.accept)
+        self.rejectBtn.clicked.connect(self.reject)
+
+        self.buttonLayout.addWidget(self.acceptBtn, alignment=Qt.AlignmentFlag.AlignRight)
+        self.buttonLayout.addWidget(self.rejectBtn, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        self.setLayout(self.mainLayout)
+
+
+class _InputDialog(QDialog):
+    def __init__(
+            self, __parent,
+            __message: str,
+            pasted_text: str = "", place_holder_text: str = "",
+            accept_title: str = "Ok", reject_title: str = "Cancel"
+    ) -> None:
+        super().__init__(__parent, f=Qt.WindowType.FramelessWindowHint)
+
+        self.setStyleSheet(FileLoader.load_style("scr/styles/dialog.css"))
+
+        self.mainLayout = QVBoxLayout()
+        self.buttonLayout = QHBoxLayout()
+
+        self.mainLayout.addWidget(
+            QLabel(__message), alignment=Qt.AlignmentFlag.AlignCenter
+        )
+
+        # input line
+        self.inputLine = QLineEdit()
+        self.inputLine.setText(pasted_text)
+        self.inputLine.setPlaceholderText(place_holder_text)
+        self.mainLayout.addWidget(self.inputLine)
+
+        # buttons
         self.mainLayout.addLayout(self.buttonLayout)
 
         self.acceptBtn = QPushButton(accept_title)
